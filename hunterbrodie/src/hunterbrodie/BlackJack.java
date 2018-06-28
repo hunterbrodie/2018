f53dddbd9f191128ff6fa022e5ad1f9d13dd5325
SummerTech <SummerTech@10.144.4.146>
2018-06-28 13:30:57 -0400

    almost finished with BlackJack

--- hunterbrodie/src/hunterbrodie/BlackJack.java
+++ hunterbrodie/src/hunterbrodie/BlackJack.java
@@ -18,67 +18,27 @@ public class BlackJack {
 		for (int x = 0; x < 2; x++)
 		{
 			dealhand.add(deck.deal());
-		}
-
-		card.setValue(1);
-		playerhand.add(card);
-		card.setValue(13);
-		playerhand.add(card);
-		print(dealhand, playerhand, 1, 2);
-
-		boolean turn = true;
-		boolean dturn = true;
-		int pcardnum = 2;
-		int upcard = 1;
-		while(dturn)
-		{
-			if(turn == true)
-			{
-				if(check(playerhand) == 2)
-				{
-					System.out.println("Player got BlackJack!");
-					turn = false;
-					dturn = false;
-					break;
-				}
-				if(check(dealhand) == 2)
-				{
-					System.out.println("Dealer got BlackJack!");
-					turn = false;
-					dturn = false;
-					break;
-				}
-				System.out.println();
-				System.out.println("Hit or Stay");
-				String hors = scan.nextLine();
-				if(hors.toLowerCase().equals("hit"))
-				{
-					playerhand.add(deck.deal());
-					print(dealhand, playerhand, upcard, pcardnum);
-					pcardnum++;
-				}
-				else
-				{
-					turn = false;
-				}
-
-			}
-			else
-			{
-				int size = handsize(dealhand);
-				if(size >= 17)
-				{
-					dturn = false;
-				}
-				else
-				{
-					dealhand.add(deck.deal());
-					upcard++;
-				}
-			}
+			playerhand.add(deck.deal());
 		}
 		
-		print(dealhand, playerhand, upcard, pcardnum);
+		while (true)
+		{
+			int x = check(playerhand);
+			int y = check(dealhand);
+			if (x == 2 && y == 2)
+			{
+				System.out.println("Both have BlackJack, both tie");
+				System.exit(0);
+			}
+			wincheck("Player", x);
+			wincheck("Dealer", y);
+			print(dealhand, playerhand, dealhand.size() - 1, playerhand.size());
+			pturn(playerhand, deck);
+			dturn(dealhand, deck);
+			System.out.println();
+			int z = playerhand.size();
+			int i = dealhand.size();
+		}
 
 	}
 
@@ -91,6 +51,11 @@ public class BlackJack {
 		}
 
 		System.out.println();
+		playerprint(playerhand, pcard);
+	}
+
+	public static void playerprint(ArrayList<Card> playerhand, int pcard)
+	{
 		System.out.println("Your cards are:");
 		for (int x = 0; x < pcard; x++)
 		{
@@ -101,7 +66,7 @@ public class BlackJack {
 	public static int check(ArrayList<Card> hand)
 	{
 		int y = handsize(hand);
-		
+
 		if(y > 21)
 		{
 			return 0;
@@ -115,31 +80,111 @@ public class BlackJack {
 			return 1;
 		}
 	}
-	
+
 	public static int handsize(ArrayList<Card> hand)
 	{
-		Card check = new Card(0, "");
 		int size = 0;
-		
+		int valcheck = 0;
+		int oval = 0;
+
 		for (int x = 0; x < hand.size(); x++)
 		{
-			int asize = 0;
-			if (hand.get(x).getValue() > 10)
+			if (hand.get(x).getValue() == 1)
 			{
-				asize = 10;
+				valcheck = 1;
 			}
-			else if (hand.get(x).getValue() == 1)
+			else if(hand.get(x).getValue() > 10)
 			{
-				asize = 11;
+				valcheck = 2;
+				oval = hand.get(x).getValue();
+			}
+			size += hand.get(x).getValue();
+		}
+
+		size = valcheck(valcheck, size, oval);
+
+		return size;
+	}
+
+	public static int valcheck(int val, int other, int oval)
+	{
+		if (val == 1)
+		{
+			other--;
+			if (other > 10)
+			{
+				return other + 1;
 			}
 			else
 			{
-				asize = hand.get(x).getValue();
+				return other + 11;
 			}
-			size += asize;
 		}
-		
-		return size;
+		else if (val == 2)
+		{
+			other = other - oval;
+			int x = other + 10;
+			return x;
+		}
+		else
+		{
+			return other;
+		}
+	}
+
+	public static ArrayList<Card> dturn(ArrayList<Card> hand, Deck deck)
+	{
+		boolean turn = true;
+		while (turn)
+		{
+			if (handsize(hand) < 17)
+			{
+				hand.add(deck.deal());
+			}
+			else
+			{
+				turn = false;
+			}
+
+		}
+
+		return hand;
+	}
+
+	public static ArrayList<Card> pturn(ArrayList<Card> hand, Deck deck)
+	{
+		Scanner scan = new Scanner(System.in);
+		boolean turn = true;
+
+		while (turn)
+		{
+			System.out.println();
+			System.out.println("Hit or Stay?");
+			if (scan.nextLine().toLowerCase().equals("hit"))
+			{
+				hand.add(deck.deal());
+				playerprint(hand, hand.size());
+			}
+			else 
+			{
+				turn = false;
+			}
+
+		}
+
+		return hand;
+	}
+	
+	public static void wincheck(String player, int x)
+	{
+		switch(x + 1)
+		{
+		case 1: System.out.println(player + " went bust, " + player +" Loses");
+		System.exit(0);
+		break;
+		case 3: System.out.println(player + " has BlackJack, " + player + " Wins");
+		System.exit(0);
+		}
 	}
 
 }
