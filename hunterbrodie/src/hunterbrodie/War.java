2187b81fe35f68c68ca6fdf7fe7b1efbeef5b69d
SummerTech <SummerTech@10.144.4.146>
2018-06-29 11:01:18 -0400

    continued war

--- hunterbrodie/src/hunterbrodie/War.java
+++ hunterbrodie/src/hunterbrodie/War.java
@@ -11,8 +11,12 @@ public class War {
 
 		ArrayList<Card> c1 = new ArrayList<Card>();
 		ArrayList<Card> c2 = new ArrayList<Card>();
-
-		for (int x = 0; x < 26; x++)
+		
+		Card buffer= new Card(0, "buffer");
+		c1.add(buffer);
+		c2.add(buffer);
+		
+		for (int x = 1; x < 27; x++)
 		{
 			c1.add(deck.deal());
 			c2.add(deck.deal());
@@ -22,38 +26,84 @@ public class War {
 
 	}
 
-	public static int turn(ArrayList<Card> c1, ArrayList<Card> c2)
+	public static int value(ArrayList<Card> c1, ArrayList<Card> c2)
 	{
-		Card c1card = c1.get(0);
-		Card c2card = c2.get(0);
-		c1.remove(0);
-		c2.remove(0);
-
-		if(c1card.getValue() > c2card.getValue())
+		if(c1.get(1).getValue() > c2.get(1).getValue())
 		{
-			c1.add(c1card);
-			c1.add(c2card);
+			return 1;
 		}
-		else if(c1card.getValue() < c2card.getValue())
+		else if(c1.get(1).getValue() < c2.get(1).getValue())
 		{
-			c2.add(c1card);
-			c2.add(c2card);
+			return 2;
 		}
-
-		if (c1.size() == 0)
+		else
 		{
-			System.out.println("Computer 2 Wins");
-			return 0;
+			return 3;
 		}
-		else if (c2.size() == 0)
+	}
+
+	public static void turn(ArrayList<Card> c1, ArrayList<Card> c2)
+	{
+		boolean turn = true;
+
+		while(turn)
 		{
-			System.out.println("Computer 1 Wins");
-			return 0;
+			int value = value(c1, c2);
+
+			switch(value)
+			{
+			case 1:
+				c1.add(c1.get(1));
+				c1.add(c2.get(1));
+				c1.remove(1);
+				c2.remove(1);
+				break;
+			case 2:
+				c2.add(c1.get(1));
+				c2.add(c2.get(1));
+				c1.remove(1);
+				c2.remove(1);
+				break;
+			case 3:
+				int value1 = value(c1, c2);
+
+				switch(value1)
+				{
+				case 1:
+					c1.add(c1.get(1));
+					c1.add(c2.get(1));
+					c1.remove(1);
+					c2.remove(1);
+					break;
+				case 2:
+					c2.add(c1.get(1));
+					c2.add(c2.get(1));
+					c1.remove(1);
+					c2.remove(1);
+				}
+
+				for (int x = 0; x < 4; x ++)
+				{
+					c1.add(c1.get(1));
+					c1.add(c2.get(1));
+					c1.remove(1);
+					c2.remove(1);
+				}
+			}
+
+
+			if (c1.size() == 0)
+			{
+				System.out.println("Computer 2 Wins");
+				turn = false;
+			}
+			else if (c2.size() == 0)
+			{
+				System.out.println("Computer 1 Wins");
+				turn = false;
+			}
+
 		}
-
-		turn(c1, c2);
-
-		return 0;
 
 	}
 
